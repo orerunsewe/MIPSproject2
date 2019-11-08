be38e8e3daad61ab81d7913ab996d611c18a7154
Ore <ore_runsewe@outlook.com>
2019-11-08 02:32:50 -0500

    Created space 1001 bytes in memory for user input, stored string to print for invalid inputs and wrote syscall for reading input string

---
+++ project2.s
@@ -0,0 +1,10 @@
+. .data
+        input_str: .space   1001                   # Preallocate space for 1000 characters and the null string
+        invalid: .asciiz    "Invalid Input"        # Store and null-terminate the string to be printed for invalid inputs
+
+.text
+        main:
+              li $v0, 8                            # Systemcall to get the user's input
+              la $a0, input_str                    # Load register with the address of the input string
+              li $a1, 1001                         # Read maximum of 1001 characters from user input (including null character)
+              syscall 
