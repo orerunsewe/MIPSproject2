c216c3bd82a076b96e58a7c2a13cd415d483efff
Ore <ore_runsewe@outlook.com>
2019-11-09 21:11:35 -0500

    Completed syscall to print invalid message and added comments to explain subroutines. Also started Loop2 to find end of string.

--- project2.s
+++ project2.s
@@ -26,18 +26,28 @@
                     add $t1, $t0, $s0                    # Get the current character's address
                     lb $t2, 0($t1)                       # Load register $t2 with the current character
                     beq $t2, $s1, PrintInvalid           # If current char is the null char, the string is empty. Therefore, invalid
-                    beq $t2, $s2, PrintInvalid             # If current char is the newline char, the string is empty. Therefore, invalid
-                    bne $t2, $s3, CheckTab                # If the current char is not a space character, go to subroutine to check if it's a tab
+                    beq $t2, $s2, PrintInvalid           # If current char is the newline char, the string is empty. Therefore, invalid
+                    bne $t2, $s3, CheckTab               # If the current char is not a space character, go to subroutine to check if it's a tab
                     addi $t0, $t0, 1                     # If the current char is a space, increment $t0 to check next character
                     j Loop1                              # Jump back to beginning of the loop
 
+              # This subroutine checks if the current chacter is a tab only when it is not a space
               CheckTab:
-                    bne $t2, $s4, SetStartIndex            # If the current char is not a tab, then set char as the start index
+                    bne $t2, $s4, SetStartIndex          # If the current char is not a tab, then set char as the start index
                     addi $t0, $t0, 1                     # Else, increment the $t0 register to check for spaces and/or tabs in the next character
                     j Loop1                              # Jump back to Loop1 to check next character
 
+              # This subroutine sets the set index after looping through all leading spaces and tabs
               SetStartIndex:
                     add $t3, $t0, $zero                  # Load register $t3 with index of first character that is not a space/tab
+                    j Loop2                              # Jump to loop 2 to check for the end of the string
 
-
+              # This subroutine prints the string "Invalid Input" for invalid user inputs.
               PrintInvalid:
+                    li $v0, 4                            # Load register with immediate 4 to print a string
+                    la $a0, invalid                      # Load address $a0 with the memory address of the string labeled 'invalid'
+                    syscall
+
+                # This subroutine checks for the index of the last character in the string by checking for the null or newline characters
+                Loop2:
+                
