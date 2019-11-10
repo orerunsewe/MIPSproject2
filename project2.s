9d32eb8fabb6e7b248dfa07fad63d170956e83e6
Ore <ore_runsewe@outlook.com>
2019-11-09 19:43:41 -0500

    Completed Loop1 and CheckTab. Started working on SetStartIndex subroutine

--- project2.s
+++ project2.s
@@ -2,6 +2,7 @@
         input_str:  .space      1001                   # Preallocate space for 1000 characters and the null string
         invalid:    .asciiz     "Invalid Input"        # Store and null-terminate the string to be printed for invalid inputs
 
+
 .text
         main:
               li $v0, 8                            # Systemcall to get the user's input
@@ -19,8 +20,13 @@
                     beq $t2, 0, PrintInvalid             # If current char is the null char, the string is empty. Therefore, invalid
                     beq $t2, 10, PrintInvalid            # If current char is the newline char, the string is empty. Therefore, invalid
                     bne $t2, 32, CheckTab                # If the current char is not a space character, go to subroutine to check if it's a tab
-
+                    addi $t0, $t0, 1                     # If the current char is a space, increment $t0 to check next character
+                    j Loop1                              # Jump back to beginning of the loop
 
               CheckTab:
                     bne $t2, 9, SetStartIndex            # If the current char is not a tab, then set char as the start index
                     addi $t0, $t0, 1                     # Else, increment the $t0 register to check for spaces and/or tabs in the next character
+                    j Loop1                              # Jump back to Loop1 to check next character
+
+              SetStartIndex:
+                    add $t3, $t0, $zero                  # Load register $t3 with index of first character that is not a space/tab
