95d56402982ad1ef75159ac9b150c9c0dfe2dcd4
Ore <ore_runsewe@outlook.com>
2019-11-11 02:21:20 -0500

    Moved initalized registers for Loop4 into a new subroutine

--- project2.s
+++ project2.s
@@ -21,9 +21,7 @@
               la $s0, input_str                    # Load register with address of user input
 
               add $t0, $zero, $zero                # Initialize counter to zero
-              add $t3, $zero, 1                    # Initialize $t3 to 1. Will be incremented by x30 in Loop4
-              addi $t7, $zero, 30                  # Load register $t7 with immediate 30 for calculations in Loop4
-              add $s7, $zero, $zero                # Initialize register $s7 for sum to calculate decimal value      
+
 
               # This subroutine is used to check for leading spaces and eliminate them by adjusting the start index of the string appropriately
               Loop1:
@@ -33,17 +31,20 @@
                     beq $t2, $s2, PrintInvalid           # If current char is the newline char, the string is empty. Therefore, invalid
                     bne $t2, $s3, CheckTab               # If the current char is not a space character, go to subroutine to check if it's a tab
                     addi $t0, $t0, 1                     # If the current char is a space, increment $t0 to check next character
+                    
                     j Loop1                              # Jump back to beginning of the loop
 
               # This subroutine checks if the current chacter is a tab only when it is not a space
               CheckTab:
                     bne $t2, $s4, SetStartIndex          # If the current char is not a tab, then set char as the start index
                     addi $t0, $t0, 1                     # Else, increment the $t0 register to check for spaces and/or tabs in the next character
+
                     j Loop1                              # Jump back to Loop1 to check next character
 
               # This subroutine sets the start index after looping through all leading spaces and tabs
               SetStartIndex:
                     add $s5, $t0, $zero                  # Load register $s5 with index of first character that is not a space/tab
+
                     j Loop2                              # Jump to loop 2 to check for the end of the string
 
               # This subroutine prints the string "Invalid Input" for invalid user inputs.
@@ -61,11 +62,13 @@
                     beq $t2, $s1, StringEnd              # If the current char is the null char, go to StringEnd
                     beq $t2, $s2, StringEnd              # If the current char is the newline char, go to StringEnd
                     addi $t4, $t4, 1                     # Increment counter to check next character
+
                     j Loop2                              # Restart Loop
 
                 # This subroutine keeps track of the end of string
                 StringEnd:
                     add $t6, $t2, $zero                  # Load the $t6 register with the index at the end of string
+
                     j Loop3                              # Jump to Loop 3 to skip trailing space and tab characters
 
                 # This subroutine skips trailing spaces and tabs in input string iterating backwards from the index calculated at StringEnd
@@ -74,18 +77,27 @@
                     lb $t2, 0($t5)                       # Load the register with the last char in the string
                     bne $t2, $s3, CheckTab2              # If current char is not a space char, check if it is a tab char
                     addi $t6, $t6, -1                    # Decrement last char in string by 1 to keep checking for non space/tab char
+
                     j Loop3                              # Restart Loop
 
                 # This subroutine checks if the current char is a tab if it is not a space. Used for eliminating trailing tabs
                 CheckTab2:
                     bne $t2, $s4, SetEndIndex             # If current char is also not a tab, set as end index for string
                     addi $t6, $t6, -1                     # Decrement register storing the end of the string by -1
+
                     j Loop3                               # Jump back to Loop3
 
                 # This subroutine sets the end index after looping through all trailing spaces and tabs
                 SetEndIndex:
                     add $s6, $t6, $zero                   # Store the end index in register $s6
-                    j Loop4                               # Jump to Loop4
+                    j Initialize                          # Jump to Loop4
+
+                Initialize:
+                add $t0, $zero, 1                    # Initialize $t3 to 1. Will be incremented by x30 in Loop4
+                addi $t1, $zero, 30                  # Load register $t7 with immediate 30 for calculations in Loop4
+                add $s7, $zero, $zero                # Initialize register $s7 for sum to calculate decimal value
+
+                j Loop4
 
                 # This subroutine loops through each character and does the calculations to determine whether the string is invalid or valid
                 # If the string is invalid, the string "Invalid Input" is printed and the program exits
